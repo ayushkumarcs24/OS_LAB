7e461af63ef7933338dda84f922f524f257dcd71
ayushkumarcs24 <ayushkumar.cs24@bmsce.ac.in>
2026-05-08 10:32:31 +0530

    Add files via upload

---
+++ LAB_5/producer consumer.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+#define SIZE 5
+
+int buffer[SIZE];
+int in = 0, out = 0;
+
+sem_t empty, full, mutex;
+
+void *producer(void *arg) {
+    int item = 1;
+
+    for(int i = 0; i < 10; i++) {
+
+        sem_wait(&empty);
+        sem_wait(&mutex);
+
+        buffer[in] = item;
+        printf("Produced: %d\n", item);
+
+        in = (in + 1) % SIZE;
+        item++;
+
+        sem_post(&mutex);
+        sem_post(&full);
+
+        sleep(1);
+    }
+}
+
+void *consumer(void *arg) {
+    int item;
+
+    for(int i = 0; i < 10; i++) {
+
+        sem_wait(&full);
+        sem_wait(&mutex);
+
+        item = buffer[out];
+        printf("Consumed: %d\n", item);
+
+        out = (out + 1) % SIZE;
+
+        sem_post(&mutex);
+        sem_post(&empty);
+
+        sleep(1);
+    }
+}
+
+int main() {
+
+    pthread_t p, c;
+
+    sem_init(&empty, 0, SIZE);
+    sem_init(&full, 0, 0);
+    sem_init(&mutex, 0, 1);
+
+    pthread_create(&p, NULL, producer, NULL);
+    pthread_create(&c, NULL, consumer, NULL);
+
+    pthread_join(p, NULL);
+    pthread_join(c, NULL);
+
+    sem_destroy(&empty);
+    sem_destroy(&full);
+    sem_destroy(&mutex);
+
+    return 0;
+}
